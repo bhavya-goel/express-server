1baeb0a1b1b0a8a72badc67971132e66383242f7
bhavya-goel <bhavya.goel@successive.tech>
2019-07-22 12:20:50 +0530

    feat: pattern

--- extra/pattern/diamond.js
+++ extra/pattern/diamond.js
@@ -1,18 +1,20 @@
-var num = process.argv[2]
+'use strict';
+const num = process.argv[2]
 if ( num < 2 || num > 10){
     console.log("Please enter integer between 2 to 10")
 }
 else{
     // for top half pyramid
-for( var i = 1; i <= num; i++){
+
+for( let i = 1; i <= num; i++){
     //to print spaces
 
-    for ( var j = i; j < num; j++){
+    for ( let j = i; j < num; j++){
         process.stdout.write(" ")
     }
     // to print pattern
 
-    for( var k = 1; k <= i; k++){
+    for( let k = 1; k <= i; k++){
         process.stdout.write("* ")
     }
     process.stdout.write("\n")
@@ -20,16 +22,16 @@ for( var i = 1; i <= num; i++){
 
 // for bottom half pyramid
 
-for( var i = 1; i <= num; i++){
+for( let i = 1; i <= num; i++){
     // to print spaces
 
-    for ( var j = i; j > 1; j--){
+    for ( let j = i; j > 1; j--){
         process.stdout.write(" ")
     }
 
     // to print pattern
 
-    for( var k = i; k <= num; k++){
+    for( let k = i; k <= num; k++){
         process.stdout.write("* ")
     }
     process.stdout.write("\n")
--- extra/pattern/equilateral.js
+++ extra/pattern/equilateral.js
@@ -1,19 +1,20 @@
-var num = process.argv[2]
+'use strict'
+let num = process.argv[2]
 
 if ( num < 2 || num > 10){
     console.log("Please enter integer between 2 to 10")
 }
 else{
     // to count rows
-for( var i = 1; i <= num; i++){
+for( let i = 1; i <= num; i++){
     //to print spaces
 
-    for ( var j = i; j < num; j++){
+    for ( let j = i; j < num; j++){
         process.stdout.write(" ")
     }
     // to print pattern
 
-    for( var k = 1; k <= i; k++){
+    for( let k = 1; k <= i; k++){
         process.stdout.write("* ")
     }
     process.stdout.write("\n")
