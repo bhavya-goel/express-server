04686776e6d674747c63fb39f3dd95d4659c80d9
bhavya-goel <bhavya.goel@successive.tech>
2019-07-24 13:46:52 +0530

    feat: validate user

--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -1,19 +1,7 @@
-const users = 
-[
-    {traineeEmail:"trainee1@successive.tech",reviewerEmail:"reviewer1@successive.tech"},
-    {traineeEmail:"traINee2@successive.tech",reviewerEmail:"revi2ewer@successive.tech"},
-    {traineeEmail:"train-ee@successive.tech",reviewerEmail:"reviewer@successive.tech"},
-    {traineeEmail:"trai.nee9@successive.tech",reviewerEmail:"reviewer.tr@successive.tech"},
-    {traineeEmail:"trainee_6@successive.tech",reviewerEmail:"reviewer_6@successive.tech"},
-    {traineeEmail:"trainee7@successive.tech",reviewerEmail:"reviewer7@successive.tech"}
-]
+import { validateEmail } from "./helpers.js"
 
-function validateEmail(email){
-    let pattern = /^[a-zA-Z0-9]+@successive\.tech$/
-    return pattern.test(email)
-}
 
-function validateUsers(users){
+export default function validateUsers(users){
     const validUser = users.filter( (value)=> {
         return (validateEmail(value.traineeEmail) && validateEmail(value.reviewerEmail))
 
@@ -26,5 +14,5 @@ function validateUsers(users){
     console.log("-----------------------------------------------------------------------")
     console.log("Invalid user count = ", inValidUser.length , "\n valid users are :" ,inValidUser)
 }
-validateUsers(users);
+//validateUsers(users);
 
