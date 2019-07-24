f234b66c3e7563fff9a7f03fe21f1c5679389026
bhavya-goel <bhavya.goel@successive.tech>
2019-07-24 15:14:29 +0530

    feat: create index file

--- extra/index.js
+++ extra/index.js
@@ -1,6 +1,6 @@
 import {equilateral, diamond} from "./pattern/index.js"
 import {validateUsers, hasPermissions} from "./utils/index.js"
-import {permissions, users} from "./constants.js"
+import {users} from "./constants.js"
 equilateral(process.argv[2])
 diamond(process.argv[3])
 validateUsers(users)
