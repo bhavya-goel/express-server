9ee286739ad177787b958f61ba4fd46614262574
bhavya-goel <bhavya.goel@successive.tech>
2019-07-24 18:32:30 +0530

    feat: validate users

--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -14,5 +14,5 @@ export default function validateUsers(users){
     console.log("-----------------------------------------------------------------------")
     console.log("Invalid user count = ", inValidUser.length , "\n valid users are :" ,inValidUser)
 }
-//validateUsers(users);
+
 
