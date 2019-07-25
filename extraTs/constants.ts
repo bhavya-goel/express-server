af2d5d4647a9891c419f14f9379b5a46f403e8a1
bhavya-goel <bhavya.goel@successive.tech>
2019-07-25 16:59:14 +0530

    feat: add constants

---
+++ extraTs/constants.ts
@@ -0,0 +1,18 @@
+import { Ipermissions, Iusers } from './interfaces';
+export const permissions: Ipermissions = {
+    getUsers: {
+        all: ['head-trainer'],
+        delete: [],
+        read : ['trainee', 'trainer'],
+        write : ['trainer'],
+    },
+};
+export const users: Iusers[] =
+[
+    {traineeEmail: 'trainee1@successive.tech', reviewerEmail: 'reviewer1@successive.tech'},
+    {traineeEmail: 'traINee2@successive.tech', reviewerEmail: 'revi2ewer@successive.tech'},
+    {traineeEmail: 'train-ee@successive.tech', reviewerEmail: 'reviewer@successive.tech'},
+    {traineeEmail: 'trai.nee9@successive.tech', reviewerEmail: 'reviewer.tr@successive.tech'},
+    {traineeEmail: 'trainee_6@successive.tech', reviewerEmail: 'reviewer_6@successive.tech'},
+    {traineeEmail: 'trainee7@successive.tech', reviewerEmail: 'reviewer7@successive.tech'},
+];
