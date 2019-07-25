f3d39f39e4170f5b4ff3460cdb3314064ed8e2e8
bhavya-goel <bhavya.goel@successive.tech>
2019-07-25 17:00:24 +0530

    feat: draw triangle

---
+++ extraTs/pattern/equilateral.ts
@@ -0,0 +1,21 @@
+
+export default function equilateral(num: number) {
+    'use strict';
+    if ( num < 2 || num > 10 || num === undefined) {
+        console.log('Please enter integer between 2 to 10 to print equilateral triangle');
+    }
+    else {
+        // to count rows
+        for ( let i: number = 1; i <= num; i++) {
+            // to print spaces
+            for ( let j: number = i; j < num; j++) {
+                process.stdout.write(' ');
+            }
+            // to print pattern
+            for ( let k: number = 1; k <= i; k++) {
+                process.stdout.write('* ');
+            }
+            process.stdout.write('\n');
+        }
+    }
+}
