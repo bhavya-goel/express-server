d52c0df0ea9e4577b0bfe1aa15be44e5243954dd
bhavya-goel <bhavya.goel@successive.tech>
2019-07-26 17:44:19 +0530

    feat: add server file

---
+++ src/Server.ts
@@ -0,0 +1,30 @@
+import express = require('express');
+import { IConfig } from './config'
+const app = express();
+export class server {
+      constructor(private config: IConfig){
+      this.config = config;
+   }
+   
+   bootstrap(){
+      this.setupRoutes()
+      return this
+   }
+   setupRoutes(){
+      
+      app.get('/health-check', function(req, res){
+         res.send('I am OK'); 
+      });
+      this.run()  
+   }
+   run(){
+      let {port,} = this.config;
+      app.listen(port,() => {
+         console.log("server running");
+      })
+      return this;
+   }
+   
+
+}
+ 
