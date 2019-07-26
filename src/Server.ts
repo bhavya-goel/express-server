1a731edae2dd5ace634daaa770ede29a366a5e7d
bhavya-goel <bhavya.goel@successive.tech>
2019-07-26 18:25:31 +0530

    feat: add server file

--- src/Server.ts
+++ src/Server.ts
@@ -1,30 +1,25 @@
-import express from 'express';
-import { IConfig } from './config'
+import express = require ('express');
+import { IConfig } from './config';
 const app = express();
-export class server {
-      constructor(private config: IConfig){
+export class Server {
+      constructor(private config: IConfig) {
       this.config = config;
    }
-   
-   bootstrap(){
-      this.setupRoutes()
-      return this
-   }
-   setupRoutes(){
-      
-      app.get('/health-check', function(req, res){
-         res.send('I am OK'); 
-      });
-      this.run()  
-   }
-   run(){
-      let {port} = this.config;
-      app.listen(port,() => {
-         console.log("server running");
-      })
+   public bootstrap() {
+      this.setupRoutes();
       return this;
    }
-   
-
+   public setupRoutes() {
+      app.get('/health-check', (req, res) => {
+         res.send('I am OK');
+      });
+      this.run();
+   }
+   public run() {
+      const {port} = this.config;
+      app.listen(port, () => {
+         console.log('server running');
+      });
+      return this;
+   }
 }
- 
