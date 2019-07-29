deb4dac28e55b97db35c4b917cdb55982d5d169c
bhavya-goel <52951403+bhavya-goel@users.noreply.github.com>
2019-07-29 09:39:15 +0530

    fix: delete compiled file

--- src/config/configuration.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var dotenv_1 = require("dotenv");
-dotenv_1.config();
-console.log(dotenv_1.config());
-var envar = process.env;
-exports.configuration = Object.freeze({
-    port: envar.PORT
-});
