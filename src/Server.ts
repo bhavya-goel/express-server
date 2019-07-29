03beaf760cab73cf0afca8edd666f01ae449661f
bhavya-goel <bhavya.goel@successive.tech>
2019-07-29 14:18:46 +0530

    feat: add middlewares

--- src/Server.ts
+++ src/Server.ts
@@ -1,19 +1,32 @@
+import * as bodyParser from 'body-parser'
 import * as express from 'express';
 import { IConfig } from './config';
+import { routeNotFound } from './libs/routes/notFoundRoute'
+import { errorHandlerMsg } from './libs/routes/errorHandler'
 export class Server {
-   private app = express();
-   private config: IConfig;
-      constructor(config: IConfig) {
-      this.config = config;
+   private app;
+      constructor(private config: IConfig) {
+      this.app = express();
    }
    public bootstrap() {
+      this.initBodyParser();
       this.setupRoutes();
       return this;
    }
+   public initBodyParser() {
+      const { app } = this;
+      app.use(bodyParser.json())
+      app.use(bodyParser.urlencoded({ extended : false}))
+      
+   }
    public setupRoutes() {
-      this.app.get('/health-check', (req, res) => {
+      const { app } = this;
+      app.use('/health-check', (req, res) => {
          res.send('I am OK');
+         console.log("health-check")
       });
+      app.use(routeNotFound)
+      app.use(errorHandlerMsg);
       this.run();
    }
    public run() {
