00efb1a775cf906f3a786a1f1c1bff4f12208e0b
bhavya-goel <bhavya.goel@successive.tech>
2019-07-30 14:01:02 +0530

    style: add comments

--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -3,10 +3,12 @@ import { validateEmail } from "./helpers.js"
 
 export default function validateUsers(users){
     const validUser = users.filter( (value)=> {
+        // returns valid users in a new array
         return (validateEmail(value.traineeEmail) && validateEmail(value.reviewerEmail))
 
     });
     const inValidUser = users.filter( (value)=> {
+        // returns invalid users in a new array
         return !(validateEmail(value.traineeEmail) && validateEmail(value.reviewerEmail))
 
     });
