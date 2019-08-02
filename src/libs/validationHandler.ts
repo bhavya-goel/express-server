ac9984441a4dcfad55cc2def2a8da2d1871faf54
bhavya-goel <bhavya.goel@successive.tech>
2019-08-02 15:51:48 +0530

    feat: add default value functionality

--- src/libs/validationHandler.ts
+++ src/libs/validationHandler.ts
@@ -1,55 +1,67 @@
 const validationHandler = (config) => (req, res, next) => {
-    console.log(req.params.id)
-        for(let key in config){
-            let flag = 0;
+    let key;
+        for (key in config){
             for(let index in config[key]){
                 let { in : place} = config[key]
                 switch(index){
+                    // checks if key present or not
                     case 'required':
-                    if((key in req[place]) && req[place][key] != null){
+                        // checks if key in request
+                        if((key in req[place]) && req[place][key] != null){
+                            break;
+                        }
+                        // if required is false and no value passed then set default value
+                        else if(config[key][index] === false){
+                            Object.defineProperty(req[place],key,{
+                                value: config[key]['default']
+                            })
+                            break;
+                        }
+                        // if key not passed and custom error function present
+                        else if((config[key]['errorMessage'] === undefined) && 'custom' in config[key]){
+                            config[key]['custom'](2);
+                            break;
+                        }
+                        // if key not passed and no error message present
+                        else if(config[key]['errorMessage'] === undefined){
+                            next(`${key} required`)
+                        }
+                        // throw error if key not present
+                        next(config[key]['errorMessage'])
                         break;
-                    }
-                    else if(config[key][index] === false){
-                        flag =1;
-                        break;
-                    }
-                    else if((config[key]['errorMessage'] === undefined) && 'custom' in config[key]){
-                        config[key].custom(req[place][key]);
-                        break;
-                    }
-                    next(config[key]['errorMessage'])
-                    break;
+                    // checks if key is alphanumeric or not
                     case 'regex':
                         let pattern = /^[a-zA-Z0-9]+$/
-                        if(pattern.test(req[place][key]) && (flag != 1)){
+                        if(pattern.test(req[place][key])){
                             break;
                         }
                         else{
                             next('enter a alphanumeric name')
                             break;
                         }
+                    // checks if key is string or not    
                     case 'string':
-                            if((typeof(req[place][key]) === 'string') && (flag != 1)){
+                            if((typeof(req[place][key]) === 'string')){
                                 break; 
                             }
-                            next('not a string')
+                            next(`${key} not a string`)
                             break;
+                    // checks if key is number or not
                     case 'number':
-                            if(isNaN(Number(req.query[key])) && flag != 1){
-                                next('not a number')
+                            if(isNaN(Number(req.query[key]))){
+                                next(`${key} not a number`)
                             }
                             break;
+                    // checks if key object or not
                     case 'isObject':
-                        if(typeof(req[place][key])=== 'object'){
+                        if(typeof(req[place][key]) === 'object'){
                             break;
                         }
-                        config[key].custom(req[place][key]);
+                        next(`${key} not an object`)
                         break;
-                    
                 }
             }
         }  
     next()
 };
-
 export default validationHandler;
