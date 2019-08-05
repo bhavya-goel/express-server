ba1c8d9494ba25b742af69e6069b0714a77cd197
bhavya-goel <bhavya.goel@successive.tech>
2019-08-05 17:37:06 +0530

    feat:  change route

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -3,10 +3,10 @@ import { authMiddleWare, validationHandler } from '../../libs';
 import { traineeRoutes } from './Controller';
 import { default as validation } from './validation';
 const traineeRouter = express.Router();
-traineeRouter.route('/')
+traineeRouter.route('/:id?')
                 .get(validationHandler(validation.get), authMiddleWare('getUsers', 'read'), traineeRoutes.get)
                 .post(validationHandler(validation.create), traineeRoutes.create)
-                .put(validationHandler(validation.update), traineeRoutes.update);
-                // .delete(validationHandler(validation.delete), traineeRoutes.delete);
-traineeRouter.delete('/:id?', validationHandler(validation.delete), traineeRoutes.delete);
+                .put(validationHandler(validation.update), traineeRoutes.update)
+                .delete(validationHandler(validation.delete), traineeRoutes.delete);
+//traineeRouter.delete('/:id?', validationHandler(validation.delete), traineeRoutes.delete);
 export default traineeRouter;
