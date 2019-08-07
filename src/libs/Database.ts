26c64a3d8e8ef18212b9ea23c7e36254c36e9ad2
bhavya-goel <52951403+bhavya-goel@users.noreply.github.com>
2019-08-07 13:35:32 +0530

    feat: database call

--- src/libs/Database.ts
+++ src/libs/Database.ts
@@ -1,11 +1,5 @@
 import * as mongoose from 'mongoose';
 import seedData from './seedData';
-const dbSchema = new mongoose.Schema({
-    date: Date,
-    name: String,
-});
-const dbModel = mongoose.model('dbModel', dbSchema);
-
 class Database {
     public static open(mongoUri) {
         mongoose.connect(mongoUri, { useNewUrlParser: true }, (err) => {
