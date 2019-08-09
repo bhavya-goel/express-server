d936532b24261be04d72adcc9a6d1cdb4a4ecfdf
bhavya-goel <bhavya.goel@successive.tech>
2019-08-09 13:16:17 +0530

    feat: add login functionality

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,6 +4,7 @@ config();
 const envar = process.env;
 export const configuration: IConfig = Object.freeze({
     mongoUri: envar.MONGO_URL,
+    password: envar.PASSWORD,
     port: envar.PORT,
     secretKey: envar.SECRET_KEY,
 });
--- src/controllers/user/routes.ts
+++ src/controllers/user/routes.ts
@@ -1,11 +1,10 @@
 import * as express from 'express';
-// import { authMiddleWare, validationHandler } from '../../libs';
+import { authMiddleWare, validationHandler } from '../../libs';
 import { userRoutes } from './Controller';
 // import { default as validation } from './validation';
 const userRouter = express.Router();
-userRouter.route('/:id?')
-                .get(userRoutes.get)
-                .post(userRoutes.create)
-                .put(userRoutes.update)
-                .delete(userRoutes.delete);
+userRouter.route('/login')
+                .post(userRoutes.login);
+userRouter.route('/me')
+                .get(authMiddleWare('getUsers', 'read'), userRoutes.getUser);
 export default userRouter;
