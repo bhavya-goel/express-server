ed140b16571b0c01237d579ba62d956d93b96d33
bhavya-goel <bhavya.goel@successive.tech>
2019-08-26 17:42:42 +0530

    fix: update by originalID

--- src/repositories/versionable/VersionableRepository.ts
+++ src/repositories/versionable/VersionableRepository.ts
@@ -86,7 +86,6 @@ export default class VersionableRepository
                 }
             }
             else {
-                console.log('user not found');
                 reject(result);
             }
         });
