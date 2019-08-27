1142b616f64dcd3be25f35cbf62f00ee6e4ae362
bhavya-goel <52951403+bhavya-goel@users.noreply.github.com>
2019-08-27 11:33:42 +0530

    fix: add auth middleware to post, put and delete requets

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -5,7 +5,7 @@ import { default as validation } from './validation';
 const traineeRouter = express.Router();
 traineeRouter.route('/')
                 .get(validationHandler(validation.get), authMiddleWare('getUsers', 'read'), traineeRoutes.get)
-                .post(validationHandler(validation.create), traineeRoutes.create)
-                .put(validationHandler(validation.update), traineeRoutes.update);
-traineeRouter.delete('/:id?', validationHandler(validation.delete), traineeRoutes.delete);
+                .post(validationHandler(validation.create), authMiddleWare('getUsers', 'wite'), traineeRoutes.create)
+                .put(validationHandler(validation.update),authMiddleWare('getUsers', 'write'), traineeRoutes.update);
+traineeRouter.delete('/:id?', validationHandler(validation.delete),authMiddleWare('getUsers', 'delete') traineeRoutes.delete);
 export default traineeRouter;
